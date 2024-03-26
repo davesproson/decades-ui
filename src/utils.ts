19f80203ec3d7c809944cadb9a37169ae8442fd3
Dave Sproson <dave.sproson@ncas.ac.uk>
2024-03-26 01:12:39 +0000

    Add a wrapper for localstorage keys with TTL.

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,27 @@ export const onLuxe = () => {
 export const capitalize = (s: string) => (s && s[0].toUpperCase() + s.slice(1)) || ""
 
 export const nullNaN = (x: number) => isNaN(x) ? null : x
+
+export const localStorageWithExpiry = {
+    set: (key: string, value: any, ttl: number) => {
+        const now = new Date()
+        const item = {
+            value: value,
+            expiry: now.getTime() + ttl
+        }
+        localStorage.setItem(key, JSON.stringify(item))
+    },
+    get: (key: string) => {
+        const itemStr = localStorage.getItem(key)
+        if (!itemStr) {
+            return null
+        }
+        const item = JSON.parse(itemStr)
+        const now = new Date()
+        if (now.getTime() > item.expiry) {
+            localStorage.removeItem(key)
+            return null
+        }
+        return item.value
+    }
+}
