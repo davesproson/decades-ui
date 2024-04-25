37afa983496a5827324bcfc77f115f65b6aa3914
Dave Sproson <dave.sproson@faam.ac.uk>
2024-04-25 13:14:41 +0100

    Dry the toolbox slightly.

--- src/map/toolbox.tsx
+++ src/map/toolbox.tsx
@@ -1,6 +1,19 @@
 import { Button } from "../components/buttons"
 import { OverlayBox } from "./overlayBox"
 
+const ToolBoxSection = ({title, children}: {title: string, children: React.ReactNode}) => (
+    <div>
+        <h3>{title}</h3>
+        <div className="is-flex">
+            {children}
+        </div>
+    </div>
+)
+
+const ToolBoxTitle = () => (
+    <h2 className="title is-4">Toolbox</h2>
+)
+
 const Toolbox = ({show}: {show: boolean}) => {
     const style = {
         right: 10,
@@ -16,33 +29,28 @@ const Toolbox = ({show}: {show: boolean}) => {
 
     return (
         <OverlayBox show={show} {...style}>
-            <h2 className="title is-4">Toolbox</h2>
-            <h3>Flags</h3>
-            <div className="is-flex">
+            <ToolBoxTitle />
+            <ToolBoxSection title="Flags">
                 <Button.Info {...buttonOpts}>Drop</Button.Info>
                 <Button.Success {...buttonOpts}>Add</Button.Success>
                 <Button.Danger {...buttonOpts}>Remove</Button.Danger>
-            </div>
-            <h3 className="mt-2">Measure</h3>
-            <div className="is-flex">
+            </ToolBoxSection>
+            <ToolBoxSection title="Measure">
                 <Button.Info {...buttonOpts}>From 146</Button.Info>
                 <Button.Info {...buttonOpts}>Line</Button.Info>
                 <Button.Danger {...buttonOpts}>Clear</Button.Danger>
-            </div>
-            <h3 className="mt-2">Wind</h3>
-            <div className="is-flex">
+            </ToolBoxSection>
+            <ToolBoxSection title="Wind">
                 <Button.Info {...buttonOpts}>Vane</Button.Info>
                 <Button.Info {...buttonOpts}>Drop Drifters</Button.Info>
                 <Button.Danger {...buttonOpts}>Clear Drifters</Button.Danger>
-            </div>
-
-            <h3 className="mt-2">Draw</h3>
-            <div className="is-flex">
+            </ToolBoxSection>
+            <ToolBoxSection title="Draw">
                 <Button.Info {...buttonOpts}>Line</Button.Info>
                 <Button.Info {...buttonOpts}>Circle</Button.Info>
                 <Button.Info {...buttonOpts}>Polygon</Button.Info>
                 <Button.Danger {...buttonOpts}>Clear</Button.Danger>
-            </div>
+            </ToolBoxSection>
         </OverlayBox>
     )
 }
