98bffdbbd2e133c087afe5d4754db79809cb733f
Dave Sproson <dave.sproson@ncas.ac.uk>
2024-11-19 12:59:00 +0000

    Function docs.

--- src/plot/plot.tsx
+++ src/plot/plot.tsx
@@ -1,4 +1,4 @@
-import { useRef, forwardRef, useEffect } from 'react'
+import React, { useRef, forwardRef, useEffect } from 'react'
 import { usePlot, usePlotOptions } from './hooks'
 import { PlotHeaderDash } from '../dashboard/dashboard'
 import { plotHeaderDefaults } from '../settings'
@@ -11,8 +11,19 @@ import { setQuickLookMode } from '@/redux/configSlice'
 interface PlotProps {
     parameters: string[] | null,
     loadDone?: boolean,
-    style?: any,
+    style?: React.CSSProperties,
 }
+/**
+ * A React forwardRef component that renders a plot with an optional header and a loading indicator.
+ *
+ * @param props - The properties passed to the component.
+ * @param props.loadDone - A boolean indicating if the plot has finished loading.
+ * @param props.parameters - An array of parameters to be displayed in the plot header.
+ * @param props.style - An optional style object to customize the style of the plot container.
+ * @param ref - A ref object to access the inner div element.
+ *
+ * @returns A JSX element representing the plot.
+ */
 const Plot = forwardRef((props: PlotProps, ref: React.Ref<HTMLDivElement>) => {
 
     const load = props.loadDone ? null : <Loader text="Loading plot..." />
@@ -23,7 +34,7 @@ const Plot = forwardRef((props: PlotProps, ref: React.Ref<HTMLDivElement>) => {
           />
         : null
 
-    const style = props.style || {     
+    const style: React.CSSProperties = props.style || {     
         top: "0px",
         left: "0px",
         right: "0px",
@@ -48,6 +59,17 @@ interface SimplePlotProps {
     params: string[],
     style?: any,
 }
+/**
+ * A functional component that renders a simple plot using the provided parameters.
+ * A simple plot is a single timeseries plot with no header, showing the last 5 minutes
+ * of data.
+ * 
+ * @param {SimplePlotProps} props - The properties for the SimplePlot component.
+ * @param {Object} props.params - The parameters for the plot.
+ * @param {Object} props.style - Optional custom styles for the plot container.
+ * 
+ * @returns A div element that contains the plot.
+ */
 const SimplePlot = (props: SimplePlotProps) => {
     const ref = useRef<HTMLDivElement>(null)
     const options = {
@@ -77,6 +99,36 @@ const SimplePlot = (props: SimplePlotProps) => {
 type PlotDispatcherProps = Partial<PlotURLOptions> & {
     containerStyle?: any,
 }
+/**
+ * PlotDispatcher component is responsible for rendering a plot with the given options and props.
+ * It uses various hooks to manage the plot options, loading state, and dispatching actions.
+ *
+ * @param {PlotDispatcherProps} [props] - The properties passed to the PlotDispatcher component.
+ * @returns The rendered Plot component or an empty fragment if options or props are not provided.
+ *
+ * @component
+ * @example
+ * return (
+ *   <PlotDispatcher 
+ *     containerStyle={{ width: '100%', height: '400px' }} 
+ *     someOtherProp={value} 
+ *   />
+ * )
+ *
+ * @remarks
+ * This component uses the following hooks:
+ * - `useRef` to create a reference to the plot container div.
+ * - `usePlotOptions` to generate plot options based on the provided props.
+ * - `usePlot` to manage the plot loading state.
+ * - `useDispatch` to dispatch actions to the Redux store.
+ * - `useEffect` to handle side effects related to the plot options and dispatch actions.
+ *
+ * The component dispatches the following actions:
+ * - `setQuickLookMode` to toggle quick look mode based on the presence of a job in the options.
+ * - `setQcJob` to set the QC job in the Redux store.
+ *
+ * The component conditionally renders the Plot component with the appropriate parameters and styles.
+ */
 const PlotDispatcher = (props?: PlotDispatcherProps) => {
     
     const ref = useRef<HTMLDivElement>(null)
@@ -105,8 +157,7 @@ const PlotDispatcher = (props?: PlotDispatcherProps) => {
         parameters={headerParams} 
         loadDone={loadDone} 
         style={props.containerStyle}
-    />
-    
+    /> 
 }
 
 export default PlotDispatcher
